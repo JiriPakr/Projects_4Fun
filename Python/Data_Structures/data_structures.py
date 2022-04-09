aaa836097a8e05d57fe2e9741120364318c01a33
Jiri <jiripakr@gmail.com>
2022-04-09 14:15:22 +0200

    Addition of python projects

---
+++ Python/Data_Structures/data_structures.py
@@ -0,0 +1,57 @@
+##
+
+class list():
+    #
+    pass
+
+##
+
+class linked_list():
+    #
+    pass
+
+##
+
+class stack():
+    # LIFO
+    pass
+
+##
+
+class queue():
+    # FIFO
+    pass
+
+##
+
+class hash_table():
+    # 
+    pass
+
+##
+
+class tree():
+    # 
+    pass
+
+##
+
+class heap():
+    # halda
+    pass
+
+##
+
+class graph():
+    # 
+    pass
+
+##
+
+
+#----------------------------------------------------------------------------------
+
+def main():
+    pass
+
+main()
