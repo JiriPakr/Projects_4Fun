1c3541bc28c06f883551803824b3cd893235b938
Jiri <jiripakr@gmail.com>
2022-10-10 17:25:54 +0200

    snake v0.2

--- Python/pygame/snake.py
+++ Python/pygame/snake.py
@@ -46,6 +46,7 @@ def message(msg,color):
 def main(win, width):
 
     snake_chunk = 20
+    snake_len = 0
 
     x1 = WIDTH/2 - 20
     y1 = WIDTH/2 - 20
@@ -55,6 +56,10 @@ def main(win, width):
 
     food_x = round(random.randrange(0, WIDTH - snake_chunk ) / 20.0) * 20.0
     food_y = round(random.randrange(0, WIDTH - snake_chunk ) / 20.0) * 20.0
+
+    sn_chunk_posmod_x = 0
+    sn_chunk_posmod_y = 0
+
 
     run = True
     game_over = False
@@ -72,15 +77,23 @@ def main(win, width):
                 if event.key == pygame.K_LEFT or event.key == pygame.K_a:
                     x1_move = -snake_chunk
                     y1_move = 0
+                    sn_chunk_posmod_x = snake_chunk
+                    sn_chunk_posmod_y = 0
                 elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                     x1_move = snake_chunk
                     y1_move = 0
+                    sn_chunk_posmod_x = -snake_chunk
+                    sn_chunk_posmod_y = 0
                 elif event.key == pygame.K_UP or event.key == pygame.K_w:
                     x1_move = 0
                     y1_move = -snake_chunk
+                    sn_chunk_posmod_x = 0
+                    sn_chunk_posmod_y = snake_chunk
                 elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
                     x1_move = 0
                     y1_move = snake_chunk
+                    sn_chunk_posmod_x = 0
+                    sn_chunk_posmod_y = -snake_chunk
 
         if x1 >= width or x1 <= 0 or y1 >= width or y1 <= 0:
             game_over = True
@@ -89,14 +102,18 @@ def main(win, width):
         y1 += y1_move            
 
         win.fill(WHITE) 
-        pygame.draw.rect(WIN, BLACK, [x1,y1, snake_chunk ,snake_chunk])
-        pygame.draw.rect(WIN, RED, [food_x,food_y, snake_chunk ,snake_chunk])
+        snake_head = pygame.draw.rect(WIN, BLUE, [x1, y1, snake_chunk ,snake_chunk])
+        snake_body = [snake_head]
+        snake_body_chunk = pygame.draw.rect(WIN, L_BLUE, [(x1+sn_chunk_posmod_x*snake_len), (y1+sn_chunk_posmod_y*snake_len), snake_chunk ,snake_chunk])
+        snake_body.append(snake_body_chunk)
+        food = pygame.draw.rect(WIN, RED, [food_x,food_y, snake_chunk ,snake_chunk])
         pygame.display.update()
 
         if x1 == food_x and y1 == food_y:
             food_x = round(random.uniform(0 + 2*snake_chunk, WIDTH - 2*snake_chunk ) / 20.0) * 20.0
             food_y = round(random.uniform(0 + 2*snake_chunk, WIDTH - 2*snake_chunk ) / 20.0) * 20.0
-
+            snake_len += 1
+            print(snake_body)
         clock.tick(15)
 
     if game_over == True:
---
+++ Python/pygame/test.py
@@ -0,0 +1,5 @@
+
+lst = [1,24,94]
+
+for i in range(1,len(lst)-1):
+    print(i)
